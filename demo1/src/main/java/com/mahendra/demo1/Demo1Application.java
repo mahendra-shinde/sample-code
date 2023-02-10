90dcf817d900ac5ac443a6dbc220c458fb9d5228
Mahendra Chandrakant Shinde <mahendrashinde@synergetics-india.com>
2023-02-10 17:16:04 +0530

    Update Demo1Application.java

--- demo1/src/main/java/com/mahendra/demo1/Demo1Application.java
+++ demo1/src/main/java/com/mahendra/demo1/Demo1Application.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Demo1Application {
 	static double pi = 3.14;
-	int is_done = false;
+	boolean is_done = false;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Demo1Application.class, args);
